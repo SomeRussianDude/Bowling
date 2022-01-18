76237795d2ee6369a7480cd13f6a63e44ed4e5cf
SomeRussianDude <yellow.mockingbird@gmail.com>
2022-01-18 17:30:28 +0100

    Implemented a mechanism for checking whether the pins are vertical

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    [SerializeField] private float standingThreshold = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(name + IsStanding());
+    }
+
+    public bool IsStanding()
+    {
+        var rotationInEuler = transform.rotation.eulerAngles;
+        var tiltInX = Mathf.Abs(rotationInEuler.x);
+        var tiltInZ = Mathf.Abs(rotationInEuler.z);
+        if (tiltInX < standingThreshold && tiltInZ < standingThreshold)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
