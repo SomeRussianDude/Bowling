92cbb531e502feebe94116d03600af0bfa9894cb
SomeRussianDude <yellow.mockingbird@gmail.com>
2022-01-20 16:44:29 +0100

    Implemented a mechanism for definitive counting of the standing pins in case they wobble

--- Assets/Scripts/PinSetter.cs
+++ Assets/Scripts/PinSetter.cs
@@ -7,8 +7,13 @@ using UnityEngine.UI;
 public class PinSetter : MonoBehaviour
 {
     [SerializeField] private Text standingDisplay;
+
+    private int lastStandingCount = -1;
     private bool ballEnteredBox;
-    
+    private float lastChangeTime;
+
+    public int LastStandingCount => lastStandingCount;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +24,37 @@ public class PinSetter : MonoBehaviour
     void Update()
     {
         standingDisplay.text = CountStanding().ToString();
+
+        if (ballEnteredBox)
+        {
+            CheckStanding();
+        }
     }
+
+    private void CheckStanding()
+    {
+        int currentStanding = CountStanding();
+        if (currentStanding != lastStandingCount)
+        {
+            lastChangeTime = Time.time;
+            lastStandingCount = currentStanding;
+            return;
+        }
+
+        float settleTime = 3f;
+        if ((Time.time - lastChangeTime) > settleTime)
+        {
+            PinsHaveSettled();
+        }
+    }
+
+    private void PinsHaveSettled()
+    {
+        lastStandingCount = -1;
+        ballEnteredBox = false;
+        standingDisplay.color = Color.green;
+    }
+
 
     public int CountStanding()
     {
