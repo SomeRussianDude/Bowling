a7dede01df683f8e7f85631821c84302cad385b8
SomeRussianDude <yellow.mockingbird@gmail.com>
2022-01-31 15:16:41 +0100

    Got everything ready to start creating a working ScoreMaster through TDD

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 
 public class ScoreMaster
 {
+    // Returns a list of cumulative scores like a normal score card
+    public static List<int> ScoreCumulative(List<int> rolls)
+    {
+        List<int> cumulativeScore = new List<int>();
+        int runningScore = 0;
+        foreach (var frameScore  in ScoreFrames(rolls))
+        {
+            runningScore += frameScore;
+            cumulativeScore.Add(runningScore);
+        }
+        return cumulativeScore;
+    }
+
+    // Returns a list of individual frame scores
     public static List<int> ScoreFrames(List<int> rolls)
     {
         List<int> frameList = new List<int>();
