aed1a6e107e7722c5152b209c7d61d693d15f0fd
SomeRussianDude <yellow.mockingbird@gmail.com>
2022-02-02 16:16:20 +0100

    Set up a Score Display script with two Lists: for each roll points and each frame points

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+
+    [SerializeField] private List<Text> bowls;
+
+    [SerializeField] private List<Text> frameScores;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
